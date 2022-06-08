d49f642def0612ecdd044fd5549d9f9e9ddd1233
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-06-08 22:30:07 +0530

    counter activity

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Hey from './Component/Hello.js/Hello';
 // import { Hello } from './Component/Hello.js/Hello';
+import Welcome from './Component/Welcome';
 function App() {
   return (
     <div className="App">
      <h1>Hello world</h1>
      <Hey/>
+     <Welcome/>
     </div>
   );
 }
