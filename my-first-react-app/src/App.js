57d832f61a3dc7c96f6a97a97a1ec312869eb776
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-06-08 21:50:21 +0530

    functional components

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hey from './Component/Hello.js/Hello';
+// import { Hello } from './Component/Hello.js/Hello';
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+     <Hey/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-first-react-app/src/Component/Hello.js/Hello.js
@@ -0,0 +1,13 @@
+//functional component 
+
+import React from "react";
+
+function Hello () {
+    return (
+        <h2>Hello, I am a functional component</h2>
+    );
+}
+
+export default Hello
+
+// export const Hello = () => <h2>Hello, I am a functional component</h2>
