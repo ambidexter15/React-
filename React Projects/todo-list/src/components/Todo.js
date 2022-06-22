d53ed1992cdef2e12ebf0ebe0983b4945c7d9e24
ambidexter15 <akanksha.gupta@pepcoding.com>
2022-06-22 21:44:15 +0530

    to-do list

---
+++ React Projects/todo-list/src/components/Todo.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+class Todo extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            tasks:["Read a book", "Solve a DSA question", "Learn React"]
+        }
+
+    }
+
+    render(){
+        return(
+            <>
+            {/* <div>This is a Todo Component</div> */}
+            <div>
+              <input type= "text" />
+              <button>Add Task</button>
+              <ul>
+                 {this.state.tasks.map((ele)=>(
+                     <li>
+                         <p>{ele}</p>
+                     </li>
+                 ))} 
+              </ul>
+
+            </div>
+            </>
+        )
+    }
+}
+
+export default Todo
